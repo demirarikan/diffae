7f53e22e1c3f44a0f79666d32abb23a77925fb1d
Wirales <candenizayhan@hotmail.com>
2024-01-28 13:36:42 +0100

    updated slurm script

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --job-name=diffae
-#SBATCH --output=checkpoint/diffae-%A.out # Standard output of the script (Can be absolute or relative path). %A adds the job id to the file name so you can launch the same script multiple times and get different logging files
-#SBATCH --error=checkpoints/diffae-%A.err # Standard error of the script
+#SBATCH --output=/checkpoints/diffae-%A.out # Standard output of the script (Can be absolute or relative path). %A adds the job id to the file name so you can launch the same script multiple times and get different logging files
+#SBATCH --error=/checkpoints/diffae-%A.err # Standard error of the script
 #SBATCH --time=0-24:00:00 # Limit on the total run time (format: dayshours: minutes:seconds)
 #SBATCH --gres=gpu:1 # Number of GPUs if needed
 #SBATCH --cpus-per-task=6 # Number of CPUs (Don't use more than 12/6 per GPU)
@@ -15,4 +15,4 @@ ml miniconda3 # load default miniconda and python module
 conda deactivate
 source activate env-diffae
 # run the script
-python us_training.py --login 48bbbe5c57a936ef9247a8de2537af5b86e5694e --datatype real --dataset 
+python us_training.py --login 48bbbe5c57a936ef9247a8de2537af5b86e5694e --datatype real --dataset /home/guests/demir_arikan/comp_surg/filtered
